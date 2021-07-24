67717aa6c230692e931813030366c023f1001932
ajspe <ajspe@192.168.254.19>
2021-07-23 20:51:10 -0700

    First commit

---
+++ GrpProject/src/Room.java
@@ -0,0 +1,44 @@
+
+public class Room {
+	private int roomNumber;
+	private int roomCap;
+	private boolean isAvail;
+	
+	public Room(int roomNumber, int roomCap, boolean isAvail) {
+		this.roomNumber = roomNumber;
+		this.roomCap = roomCap;
+		this.isAvail = isAvail;
+	}
+	
+	@Override
+	public String toString() {
+		return ""+ roomNumber + " " + roomCap + " " + isAvail;
+	}
+	
+	public boolean equals(Room room) {
+		return this.roomNumber == room.roomNumber;
+	}
+	
+	
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public int getRoomCap() {
+		return roomCap;
+	}
+	public void setRoomCap(int roomCap) {
+		this.roomCap = roomCap;
+	}
+	public boolean isAvail() {
+		return isAvail;
+	}
+	public void setAvail(boolean isAvail) {
+		this.isAvail = isAvail;
+	}
+	
+	
+}
---
+++ GrpProject/src/RoomSet.java
@@ -0,0 +1,49 @@
+
+public class RoomSet {
+	private static Room[] roomSet = new Room[20];
+	private static int count = 0;
+	
+	public void addRoom(Room room) {
+		roomSet[count]=room;
+		count++;
+	}
+	
+	
+	public void deleteRoom (Room room) {
+		if(roomExist(room) == true) {
+			int index = getRoomIndex(room);
+			for(int i = index; i < count - 1; i++) {
+				roomSet[i] = roomSet[i+1];
+			}
+		}
+	}
+	
+	//searches by roomNumber then replaces the entire room object
+	public void updateRoom(Room room) {
+		for(int i = 0; i < count; i++) {
+			if (roomSet[i].equals(room)) {
+				roomSet[i]=room;
+			}
+		}
+	}
+	
+	//helper method
+	public boolean roomExist(Room room) {
+		for(int i = 0; i < count; i++) {
+			if (roomSet[i].equals(room)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int getRoomIndex(Room room) {
+		for(int i = 0; i < count; i++) {
+			if (roomSet[i].equals(room)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+}
