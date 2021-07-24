36869abdf9646d1bb06eb78258d33f2092dbcd27
shrimp-wizard <87888336+shrimp-wizard@users.noreply.github.com>
2021-07-24 14:49:53 -0700

    Update Employee.java

--- GrpProject/src/Employee.java
+++ GrpProject/src/Employee.java
@@ -1,4 +1,27 @@
 
 public class Employee {
-
+  private String name;
+  private String id;
+  privete String position;
+  private String username;
+  private String pswd;
+  //hashset meetings
+  //hashset schedule
+  //hashset notifications
+  
+  constructor(NAME, ID, POSITION, USERNAME, PSWD){
+    name = NAME;
+    id = ID;
+    position = POSITION;
+    username = USERNAME;
+    pswd = PSWD;
+    meetings = new empty hashset;
+    schedule = new empty hashset;
+    notifications = new empty hashset;
+  }
+  
+  setTimeSlot(date, time){
+    //todo
+  }
+    
 }
