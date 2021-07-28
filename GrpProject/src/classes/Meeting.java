b989c064c88b4a3d9f3a3f9e7407f73ac67bf71d
ajspe <ajspe@192.168.254.19>
2021-07-27 18:21:54 -0700

    a little bit more on Meeting

--- GrpProject/src/classes/Meeting.java
+++ GrpProject/src/classes/Meeting.java
@@ -1,5 +1,12 @@
 package classes;
+import java.util.*;
 
 public class Meeting {
 	private int meetingNumber;
+	private Date date;
+	private int time;  // Military time
+	private Employee owner;
+	private Employee[] attendingSet;
+	private int attendingCount;
+	
 }
