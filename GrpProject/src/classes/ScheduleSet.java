590b4b795c904544e7a866494223d58a0255b97d
ajspe <ajspe@192.168.254.19>
2021-07-28 16:31:18 -0700

    added date to SceduleSet

--- GrpProject/src/classes/ScheduleSet.java
+++ GrpProject/src/classes/ScheduleSet.java
@@ -1,15 +1,18 @@
 package classes;
+import java.util.*;
 
 public class ScheduleSet {
 	private static int[] empId = new int[100];
 	private static int[] meetingNum = new int [100];
+	private static Date[] date = new Date[100];
 	private static int[] startTime = new int [100];
 	private static int[] endTime = new int [100];
 	private static int count = 0;
 	
-	public static void addEntry(int empKey, int meetingKey, int start, int end) {
+	public static void addEntry(int empKey, int meetingKey,Date dateEntry, int start, int end) {
 		empId[count]= empKey;
 		meetingNum[count]=meetingKey;
+		date[count] = dateEntry;
 		startTime[count]=start;
 		endTime[count]=end;
 		count++;
@@ -19,6 +22,7 @@ public class ScheduleSet {
 		if(empId[0] == empKey && meetingNum[0] == meetingKey && count == 1) {
 			empId[0]=0;
 			meetingNum[0]=0;
+			date[0] = new Date(0);
 			startTime[0]=0;
 			endTime[0]=0;
 			count--;
@@ -34,12 +38,14 @@ public class ScheduleSet {
 			if (flag == true) {
 				empId[i]=empId[i+1];
 				meetingNum[i]=meetingNum[i+1];
+				date[i]= date[i+1];
 				startTime[i]=startTime[i+1];
 				endTime[i]=endTime[i+1];
 			}
 		}
 		empId[count]=0;
 		meetingNum[count]=0;
+		date[count] = new Date(0);
 		startTime[count]=0;
 		endTime[count]=0;
 		count--;
