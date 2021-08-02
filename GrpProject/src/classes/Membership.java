000f6b1a597b53d3a6579316111aae2d36663a3d
shrimp-wizard <87888336+shrimp-wizard@users.noreply.github.com>
2021-08-01 20:15:25 -0700

    Update and rename notif to Membership.java

---
+++ GrpProject/src/classes/Membership.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Membership {
+  private Employee emp;
+  private Meeting meet;
+  
+  public membership(Employee emp; Meeting meet) {
+    this.emp = emp
+    this.meet = meet
+  }
+
+  public Employee getEmp() {
+    return emp;
+  }
+
+  public Meeting getMeet() {
+    return meet;
+  }
+}
