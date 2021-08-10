7b3cfb2aa2930a8b01b4b598e2cd59d1d3adb075
ajspe <ajspe@192.168.254.19>
2021-08-09 21:30:34 -0700

    membershipSet

--- GrpProject/src/classes/MembershipSet.java
+++ GrpProject/src/classes/MembershipSet.java
@@ -6,14 +6,26 @@ public class MembershipSet {
 	  private static boolean[] active = new boolean[100];
 	  private static int count = 0;
 	  
-	  public static void addToMembershipSet(Employee emp, Meeting meet) {
+	  /**
+	   * public static void addToMembershipSet(Employee emp, Meeting meet)
+	   * adds entry
+	 * @param emp
+	 * @param meet
+	 */
+	public static void addToMembershipSet(Employee emp, Meeting meet) {
 		  employeeSet[count] = emp;
 		  meetingSet[count] = meet;
 		  active[count]=true;
 		  count++;
 		  
 	  }
-	  public static void setInactive(Employee emp, Meeting meet) {
+	  /**
+	   * public static void setInactive(Employee emp, Meeting meet) 
+	   * set meeting inActive
+	 * @param emp
+	 * @param meet
+	 */
+	public static void setInactive(Employee emp, Meeting meet) {
 		  for(int i = 0; i < count ; i++) {
 			  if(employeeSet[i].equals(emp) && meetingSet[i].equals(meet) && active[i] == true) {
 				  active[i]=false;
@@ -21,16 +33,24 @@ public class MembershipSet {
 			  }
 		  }
 	  }
-	  public static Meeting[] fillMeetings(Employee emp) {
+	  /**
+	   * public static Meeting[] fillMeetings(Employee emp)
+	   * fills meeting in  object ram for GUI
+	 * @param emp
+	 * @return
+	 */
+	public static Meeting[] fillMeetings(Employee emp) {
+		  //create temp meeting array
 		  Meeting[] temp = new Meeting[50];
 		  int counter = 0;
+		  //loop through and find all meetings asigned
 		  for(int i = 0; i < count; i++ ) {
 			  if(employeeSet[i].equals(emp) && active[i] == true) {
 				  temp[counter]=meetingSet[i];
 				  counter++;
 			  }
 		  }
-		  
+		  //create new array with final size
 		  Meeting[] result = new Meeting[counter];
 		  for(int i = 0; i < counter; i++) {
 			  result[i]=temp[i];
@@ -38,7 +58,13 @@ public class MembershipSet {
 		  
 		  return result;
 	  }
-	  public static int getEmpCount(Employee emp) {
+	  /**
+	   * public static int getEmpCount(Employee emp) 
+	   * get index count for givin employee for GUI
+	 * @param emp
+	 * @return
+	 */
+	public static int getEmpCount(Employee emp) {
 		  Meeting[] temp = new Meeting[50];
 		  int counter = 0;
 		  for(int i = 0; i < count; i++ ) {
