6e0700fa80cdcc8bed97f3e2ded49b230a8985fb
ajspe <ajspe@192.168.254.19>
2021-08-09 19:12:12 -0700

    Meeting and Room comments

--- GrpProject/src/classes/Meeting.java
+++ GrpProject/src/classes/Meeting.java
@@ -13,8 +13,16 @@ public class Meeting {
 	
 	
 	
+	/**
+	 * this is the constructor for Meeting
+	 * @param meetingNumber
+	 * @param date
+	 * @param startTime
+	 * @param endTime
+	 * @param owner
+	 * @param room
+	 */
 	public Meeting(int meetingNumber, Date date, int startTime,int endTime, Employee owner,Room room) {
-		super();
 		this.meetingNumber = meetingNumber;
 		this.date = date;
 		this.startTime = startTime;
@@ -25,24 +33,38 @@ public class Meeting {
 		this.attendingSet = new Employee[20];
 	}
 	
+	/**
+	 * inviteEmployee(Employee emp) 
+	 * this method adds emp and meeting to notificationSet
+	 * @param emp
+	 */
 	public void inviteEmployee(Employee emp) {
 		NotificationSet.addToNotificationSet(emp, this);
 	}
 	
+	/**
+	 * public void deleteEmployee(Employee employee)
+	 * this method deletes a given employee
+	 * @param employee
+	 */
 	public void deleteEmployee(Employee employee) {
+		//handle count = 1 case
 		if(this.attendingCount == 1) {
 			if (employee.equals(this.attendingSet[0])){
 				this.attendingSet[0]= null;
 				this.attendingCount--;
 			}
 		}else {
+			//flag for determining shift
 			boolean flag = false;
 			
+			//cycle through array to delete emp by shifting the array after emp is found
 			for(int i = 0; i < (this.attendingCount - 1); i++) {
 				if (employee.equals(this.attendingSet[i])) {
 					flag = true;
 				}
 				if (flag == true) {
+					//shift array
 					this.attendingSet[i] = this.attendingSet[i+1];
 					this.attendingCount=0;
 				}
@@ -50,20 +72,29 @@ public class Meeting {
 		}
 	}
 	
+	/**
+	 * public void deleteEmployee(int id)
+	 * Deletes employee given empID
+	 * @param id
+	 */
 	public void deleteEmployee(int id) {
+		//handle count = 1 case
 		if(this.attendingCount == 1) {
 			if (this.attendingSet[0].equals(id)){
 				this.attendingSet[0]= null;
 				this.attendingCount--;
 			}
 		}else {
+			//flag for determining shift
 			boolean flag = false;
 			
+			//cycle through array to delete emp by shifting the array after emp is found
 			for(int i = 0; i < (this.attendingCount - 1); i++) {
 				if (this.attendingSet[i].equals(id)) {
 					flag = true;
 				}
 				if (flag == true) {
+					//shift array
 					this.attendingSet[i] = this.attendingSet[i+1];
 					this.attendingCount=0;
 				}
@@ -71,19 +102,30 @@ public class Meeting {
 		}
 	}
 	
+	/**
+	 * public void addEmployee (Employee employee)
+	 * adds employee to attending set
+	 * @param employee
+	 */
 	public void addEmployee (Employee employee) {
+		//add employee
 		this.attendingSet[this.attendingCount] = employee;
+		//increment count
 		this.attendingCount++;
 	}
+	
+	//getters and setters
 	
 	public int getStartTime() {
 		return startTime;
 	}
 
+	
 	public void setStartTime(int startTime) {
 		this.startTime = startTime;
 	}
 
+	
 	public int getEndTime() {
 		return endTime;
 	}
--- GrpProject/src/classes/Room.java
+++ GrpProject/src/classes/Room.java
@@ -5,33 +5,65 @@ public class Room {
 	private int roomCap;
 	private boolean isAvail;
 	
+	/**
+	 * public Room(int roomNumber, int roomCap, boolean isAvail)
+	 * constructor for room
+	 * @param roomNumber
+	 * @param roomCap
+	 * @param isAvail
+	 */
 	public Room(int roomNumber, int roomCap, boolean isAvail) {
 		this.roomNumber = roomNumber;
 		this.roomCap = roomCap;
 		this.isAvail = isAvail;
 	}
 	
+	/**
+	 *toString overide
+	 * returns ""+ roomNumber + " " + roomCap + " " + isAvail;
+	 */
 	@Override
 	public String toString() {
 		return ""+ roomNumber + " " + roomCap + " " + isAvail;
 	}
 	
+	/**public boolean equals(Room room) 
+	 * check if equal by object
+	 * @param room
+	 * @return
+	 */
 	public boolean equals(Room room) {
 		return this.roomNumber == room.getRoomNumber();
 	}
 	
+	/**
+	 * public boolean equals(int roomNumber)
+	 * check if equal by roomNumber
+	 * @param roomNumber
+	 * @return
+	 */
 	public boolean equals(int roomNumber) {
 		return this.roomNumber == roomNumber;
 	}
 	
+	/**
+	 * public boolean checkRoomCap(int num)
+	 * check if room is at cap with given int input
+	 * @param num
+	 * @return
+	 */
 	public boolean checkRoomCap(int num) {
+		//create flag
 		boolean result = true;
+		//check if at capacity
 		if(this.roomCap < num) {
 			result = false;
 		}
 		return result;
 	}
 
+	//getters and setters 
+	
 	public int getRoomNumber() {
 		return roomNumber;
 	}
